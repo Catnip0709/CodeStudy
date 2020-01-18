be04b5fa12db4a7533cd24c6725a4f611316c051
Catnip <13347629779@163.com>
2020-01-18 21:37:31 +0800

    fixPro(重建二叉树.cpp)

--- 剑指offer/重建二叉树.cpp
+++ 剑指offer/重建二叉树.cpp
@@ -8,7 +8,8 @@ Link: https://www.nowcoder.com/practice/8a19cbe657394eeaac2f6ea9b0f6fcf6?tpId=13
 /* Q
 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
-例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
+例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}
+则重建二叉树并返回。
 */
 
 /* A
@@ -18,16 +19,7 @@ Link: https://www.nowcoder.com/practice/8a19cbe657394eeaac2f6ea9b0f6fcf6?tpId=13
 
 #include<iostream>
 #include<vector>
-#include<stack>
 using namespace std;
-
-struct ListNode {
-	int val;
-	struct ListNode *next;
-	ListNode(int x) :
-	val(x), next(NULL) {
-	}
-};
 
 struct TreeNode {
 	int val;
